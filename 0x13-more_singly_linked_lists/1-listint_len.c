61a2c32356f6d369f436031e9d6845a3edfe4d24
flamonrose23 <fatinemar20@icloud.com>
2023-06-06 14:38:35 -0700

    Task 1 listint_len

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * listint_len - returning numbers of elements inthe list
+ * @h: linked list to traverse
+ *
+ * Return: number of nodes
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t n = 0;
+
+	while (h)
+	{
+		n++;
+		h = h->next;
+	}
+
+	return (n);
+}
